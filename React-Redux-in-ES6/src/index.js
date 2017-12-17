f70235450fb10eed36ddb0765442f2c522d15e42
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2017-12-17 17:13:02 +0300

    Instantiate Store and Provider

--- React-Redux-in-ES6/src/index.js
+++ React-Redux-in-ES6/src/index.js
@@ -2,11 +2,17 @@ import "babel-polyfill";
 import React from "react";
 import ReactDom from "react-dom";
 import Routes from "./routes";
+import {Provider} from "react-redux"
+import ConfigureStoreFunc from "./store/configureStore";
 import {Router, browserHistory } from "react-router";
 import "./styles/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+const store = ConfigureStoreFunc();
+
 ReactDom.render(
-    <Router history={browserHistory} routes={Routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={Routes} />
+    </Provider>,
     document.getElementById("app")
 );
