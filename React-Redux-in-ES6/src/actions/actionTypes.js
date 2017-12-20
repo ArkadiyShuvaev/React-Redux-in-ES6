528ce26e64bb9dca14a1aa58828a3b36c04551d9
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2017-12-20 11:21:47 +0300

    Create Load Courses Thunk

--- React-Redux-in-ES6/src/actions/actionTypes.js
+++ React-Redux-in-ES6/src/actions/actionTypes.js
@@ -1,2 +1,3 @@
 // ReSharper disable InconsistentNaming
 export const CREATE_COURSE = "CREATE_COURSE";
+export const LOAD_COURSES_SUCCESS = "LOAD_COURSES_SUCCESS";
--- React-Redux-in-ES6/src/actions/courseActions.js
+++ React-Redux-in-ES6/src/actions/courseActions.js
@@ -1,5 +1,21 @@
 import * as CreateActions from "./actionTypes";
+import CourseApi from "../api/mockCourseApi";
 
 export function createCourse(course) {
     return { type: CreateActions.CREATE_COURSE, course }; // course: course
+}
+
+function loadCoursesSuccess(courses) {
+    return { type: CreateActions.LOAD_COURSES_SUCCESS, courses };
+};
+
+export function loadCourses() {
+    
+    return (dispatch) => {
+        CourseApi.getAllCourses().then(courses => {
+            dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw (error);
+        });
+    };
 }
