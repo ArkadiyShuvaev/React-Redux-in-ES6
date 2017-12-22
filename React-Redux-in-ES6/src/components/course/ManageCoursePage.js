2b2dd4ef2015707bd5001f5b0d54ac35e7e1f55c
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2017-12-22 12:28:42 +0300

    Use Manage Course Form

--- React-Redux-in-ES6/src/components/course/ManageCoursePage.js
+++ React-Redux-in-ES6/src/components/course/ManageCoursePage.js
@@ -1,31 +1,40 @@
 import React, { PropTypes } from "react";
 import { connect } from "react-redux";
 import * as CourseActions from "../../actions/courseActions";
+import CourseForm from "./CourseForm.jsx";
+import * as CourseList from "./CourseList.jsx";
 
 
 class ManageCoursePage extends React.Component {
 
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        }
     }
 
     render() {
-        const { course } = this.props;
         return (
-            <div>
-                <h1>Manage Course</h1>
-
-            </div>
+            <CourseForm
+                course={this.state.course}
+                errors={this.state.errors}
+                allAuthors={[]}
+            />
         );
     }
 }
 
 ManageCoursePage.propTypes = {
-    course: PropTypes.array.isRequired
+    course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-    return { course: state.course };
+
+    let course = { id: "", watchHref: "", title: "", authorId: "", length: "", category: "" };
+
+    return { course: course };
 }
 
 function mapDispatchToProps(dispatch) {
