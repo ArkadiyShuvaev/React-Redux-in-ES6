58335440c38388a46345775ebf9de3a0c3a5e2bc
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2017-12-25 15:32:02 +0300

    Async Status and Error Handling : Hide Preloader Based on Status

--- React-Redux-in-ES6/src/components/App.js
+++ React-Redux-in-ES6/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { PropTypes } from "react";
+import { connect } from "react-redux";
 import Header from "./common/Header.jsx";
 
 class App extends React.Component {
+    
     render() {
         return (
             <div className="container-fluid">
-                <Header />
+                <Header isLoading={this.props.isLoading}  />
                 {this.props.children}
             </div>
         );
@@ -13,7 +15,13 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-    children: PropTypes.object.isRequired
+    children: PropTypes.object.isRequired,
+    isLoading: PropTypes.bool.isRequired
 };
-
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        isLoading:
+            state.ajaxCallsInProgressCount > 0
+    };
+};
+export default connect(mapStateToProps)(App);
--- React-Redux-in-ES6/src/components/common/Header.jsx
+++ React-Redux-in-ES6/src/components/common/Header.jsx
@@ -2,7 +2,12 @@ import React, { PropTypes } from "react";
 import { Link, IndexLink } from "react-router";
 import LoadingDots from "./LoadingDots";
 
-const header = () => {
+const header = ({ isLoading }) => {
+
+    var preloader = isLoading > 0
+        ? (<LoadingDots interval={100} dots={20} />)
+        : null;
+
     return (
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -10,9 +15,13 @@ const header = () => {
             <Link to="courses" activeClassName="active">Courses</Link>
             {" | "}
             <Link to="about" activeClassName="active">About</Link>
-            <LoadingDots interval={100} dots={20} />
+            {preloader}
         </nav>
     );
+};
+
+header.propTypes = {
+    isLoading: PropTypes.bool.isRequired
 };
 
 export default header;
