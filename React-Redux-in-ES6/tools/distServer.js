8bf8545011394d2c2f8775f9137924db909eabb0
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2018-01-03 21:38:25 +0300

    Production Builds : Setup Dist Server

---
+++ React-Redux-in-ES6/tools/distServer.js
@@ -0,0 +1,24 @@
+import Express from "express";
+import Path from "path";
+import Open from "open";
+import Compression from "compression";
+
+/*eslint-disable no-console */
+
+const port = 3000;
+const app = Express();
+
+app.use(Compression());
+app.use(Express.static("dist"));
+
+app.get("*", function (req, res) {
+    res.sendFile(Path.join(__dirname, "../dist/index.html"));
+});
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        Open(`http://localhost:${port}`);
+    }
+});
